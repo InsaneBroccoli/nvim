04e1b0e4560d600b4fac417a92219156d75b9dbb
Dario Jurietti <aware_lucite0l@icloud.com>
2026-03-04 11:34:16 +0100

    add 99 plugin

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+-- bootstrap lazy.nvim, LazyVim and your pluginso
+vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/nvim-treesitter")
 require("config.lazy")
